ca2605f852db0ca3766559ed11920c32153e11a4
Hadassah <hadassah.mo27@gmail.com>
2026-04-23 04:55:34 +0100

    react native project update

---
+++ Documents/MyApp/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "lightblue" }}>
+      <Text>Hello! This is my Git Assignment 2.0</Text>
+    </View>
+  );
+}
