8307d6d95804e47512fade307d73fac1f900715a
Hadassah Olatunde-Taiwo <112834740+Mohadi27@users.noreply.github.com>
2026-05-06 22:40:55 +0100

    Update index.tsx

--- Documents/MyApp/app/(tabs)/index.tsx
+++ Documents/MyApp/app/(tabs)/index.tsx
@@ -3,7 +3,13 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "lightblue" }}>
-      <Text>Hello! This is my Git Assignment 2.0</Text>
+      {/* Updated text with your full name */}
+      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+        Hello! Hadassah Olatunde-Taiwo from GIT class.
+      </Text>
+      <Text style={{ marginTop: 10 }}>
+        Modified by: Praise Oluwaseun Ajayi
+      </Text>
     </View>
   );
 }
